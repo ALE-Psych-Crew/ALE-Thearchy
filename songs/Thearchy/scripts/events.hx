9e71d1c4ba686810cff2f9bd1b1dc6b1af213f70
AlejoGDOfficial <alejogdoff@gmail.com>
2026-04-26 22:40:34 -0500

    cam

--- songs/Thearchy/scripts/events.hx
+++ songs/Thearchy/scripts/events.hx
@@ -1,3 +1,8 @@
+import flixel.addons.display.FlxBackdrop;
+
+var mask:FlxBackdrop = new FlxBackdrop();
+mask.makeGraphic(FlxG.width, FlxG.height);
+
 function onCreate()
 {
     spawnNotes = startTime <= 0;
@@ -8,6 +13,47 @@ function onCreate()
 function postCreate()
 {
     botplay = startTime > 0;
+
+    addBehindOpponents(mask);
+
+    shouldMoveCamera = false;
+
+    camHUD.alpha = 0;
+
+    stage.get('ground').color = FlxColor.fromRGB(50, 50, 50);
+
+    dad.alpha = 0;
+
+    dad.shader.brightness = bf.shader.brightness = -200;
+
+    camGame.bopModulo = 0;
+    camGame.position.set(1250, 650);
+    camGame.snapToTarget();
+    camGame.zoomSpeed = 2;
+    
+    camGame.fade(FlxColor.WHITE, 0);
 }
 
-startTime = 0.1 ?? Conductor.beatsToTime(88);
+function onSongStart()
+{
+    camGame.fade(FlxColor.WHITE, 1 * Conductor.secCrochet, true);
+
+    camGame.targetZoom = camGame.zoom = 1;
+    camGame.tweenZoom(0.5, 32 * Conductor.secCrochet, {ease: FlxEase.cubeOut});
+}
+
+function onBeatHit(curBeat:Int)
+{
+    switch (curBeat)
+    {
+        case 32:
+            camGame.cancelZoomTween();
+            camGame.targetZoom = 0.3;
+
+            shouldMoveCamera = true;
+
+            FlxTween.tween(dad, {alpha: 1}, 2 * Conductor.secCrochet, {ease: FlxEase.cubeOut});
+    }
+}
+
+startTime = Conductor.beatsToTime(30);
